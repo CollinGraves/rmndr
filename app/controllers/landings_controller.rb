c9c04705cf3de36f5596ac702ee3abd45730d29b
Prelang Builder <builder@prelang.com>
2016-02-17 19:32:27 -0600

    Executed 'bundle install' to fetch and install required Ruby Gems

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -1,4 +1,13 @@
 class LandingsController < ApplicationController
+  before_action :check_phone_cookie_value, only: [:index, :create, :show]
   def index
+
+  end
+
+
+  def check_phone_cookie_value
+    if cookies.signed[:phone_number].nil?
+      { flash: 'You need a valid phone phone number' }
+    end
   end
 end
